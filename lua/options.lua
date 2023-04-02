1fd5fcc8b1e4ec63de123c1b4b2e7e749e81694b
Zhukov Daniil <dmzhukov@outlook.com>
2023-04-02 21:52:35 +0400

    Neovide GUI options

--- lua/options.lua
+++ lua/options.lua
@@ -1,3 +1,5 @@
+----------------------------- General options --------------------------------
+--
 vim.o.encoding = 'UTF-8'
 vim.o.compatible = false
 vim.o.ignorecase = true
@@ -18,5 +20,26 @@ vim.o.completeopt = 'menu,menuone,noselect'
 vim.o.tgc = true
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+vim.o.sessionoptions = 'blank,buffers,curdir,folds,help,tabpages,winsize,' ..
+                       'winpos,terminal,localoptions'
+
+--------------------------- Neovide GUI options ------------------------------
+
+vim.o.guifont = 'mononoki Nerd Font:h16'
+
+-- Helper function for transparency formatting
+local alpha = function()
+    return string.format("%x", math.floor((255 * vim.g.transparency) or 0.8))
+end
+-- g:neovide_transparency should be 0 if you want to unify transparency of
+-- content and title bar.
+vim.g.neovide_transparency = 0.0
+vim.g.transparency = 0.8
+vim.g.neovide_background_color = "#0f1117" .. alpha()
+
+vim.g.neovide_hide_mouse_when_typing = true
+
+vim.g.neovide_remember_window_size = true
+
+vim.g.neovide_input_use_logo = false
 
