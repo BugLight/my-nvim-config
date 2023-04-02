02db0d0709fa94ad626bc87bf1120d84b144ac36
Zhukov Daniil <dmzhukov@outlook.com>
2023-04-02 22:33:17 +0400

    Update lspsaga mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -13,14 +13,21 @@ vim.api.nvim_set_keymap('', '<C-k>', '<CMD>BufferLineCyclePrev<CR>', { noremap =
 vim.api.nvim_set_keymap('', '<C-t>', '<CMD>ToggleTerm<CR>', { noremap = true, silent = true })
 
 -- substitute
-vim.keymap.set('n', 's', '<cmd>lua require(\'substitute\').operator()<cr>', { noremap = true, silent = true })
-vim.keymap.set('n', 'ss', '<cmd>lua require(\'substitute\').line()<cr>', { noremap = true, silent = true })
-vim.keymap.set('n', 'S', '<cmd>lua require(\'substitute\').eol()<cr>', { noremap = true, silent = true })
-vim.keymap.set('x', 's', '<cmd>lua require(\'substitute\').visual()<cr>', { noremap = true, silent = true })
+vim.keymap.set('n', 's', '<CMD>lua require(\'substitute\').operator()<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', 'ss', '<CMD>lua require(\'substitute\').line()<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', 'S', '<CMD>lua require(\'substitute\').eol()<CR>', { noremap = true, silent = true })
+vim.keymap.set('x', 's', '<CMD>lua require(\'substitute\').visual()<CR>', { noremap = true, silent = true })
 
 -- lspsaga
-vim.keymap.set('n', 'gh', '<cmd>Lspsaga lsp_finder<CR>')
-vim.keymap.set('n','gd', '<cmd>Lspsaga goto_definition<CR>')
+vim.keymap.set('n', 'gh', '<CMD>Lspsaga lsp_finder<CR>')
 
-vim.keymap.set('n', '<leader>ca', '<cmd>Lspsaga code_action<CR>')
-vim.keymap.set('n', '<leader>r', '<cmd>Lspsaga rename<CR>')
+vim.keymap.set('n', 'gd', '<CMD>Lspsaga goto_definition<CR>')
+vim.keymap.set('n', '<leader>gd', '<CMD>Lspsaga peek_definition<CR>')
+
+vim.keymap.set('n', 'K', '<CMD>Lspsaga hover_doc<CR>')
+
+vim.keymap.set('n', '<leader>ca', '<CMD>Lspsaga code_action<CR>')
+
+vim.keymap.set('n', '[e', '<CMD>Lspsaga diagnostic_jump_prev<CR>')
+vim.keymap.set('n', ']e', '<CMD>Lspsaga diagnostic_jump_next<CR>')
+
