4430296b862ce0a728b489b0600516ebda5877f4
buglight <dmzhukov@outlook.com>
2023-08-24 13:35:05 +0400

    Fancier buffer line

--- lua/config/cokeline.lua
+++ lua/config/cokeline.lua
@@ -1,12 +1,17 @@
 return function()
-    local get_hex = require('cokeline/utils').get_hex
+    local get_hex = require('cokeline.utils').get_hex
+    local is_picking_focus = require('cokeline.mappings').is_picking_focus
+    local is_picking_close = require('cokeline.mappings').is_picking_close
 
     require('cokeline').setup {
         buffers = {
             filter_valid = function(buffer)
-                -- Keep only current project related buffers
+                -- Keep only current project related buffers unless buffer is
+                -- modified
                 local cwd = vim.fn.getcwd()
-                return buffer.path:find(cwd, 1, true) == 1
+                return buffer.is_focused
+                    or buffer.is_modified
+                    or buffer.path:find(cwd, 1, true) == 1
             end,
             new_buffers_position = 'directory',
         },
@@ -16,22 +21,51 @@ return function()
         history = {
             size = 5,
         },
+        default_hl = {
+            fg = function(buffer)
+                return buffer.is_focused
+                    and get_hex('Normal', 'fg')
+                    or get_hex('Pmenu', 'fg')
+            end,
+            bg = function(buffer)
+                return buffer.is_focused
+                    and get_hex('Normal', 'bg')
+                    or get_hex('Pmenu', 'bg')
+            end
+        },
         components = {
             {
-                text = function(buffer) return (buffer.index ~= 1) and '▏' or '' end,
+                text = '▏',
                 fg = get_hex('Normal', 'fg'),
             },
             {
-                text = function(buffer) return '    ' .. buffer.devicon.icon end,
+                text = function(buffer)
+                    return ((is_picking_focus() or is_picking_close())
+                        and buffer.pick_letter
+                        or buffer.index) .. ':  '
+                end,
+                fg = function()
+                    return (is_picking_focus() or is_picking_close())
+                        and get_hex('ErrorMsg', 'fg')
+                        or get_hex('Normal', 'fg')
+                end
+            },
+            {
+                text = function(buffer) return buffer.devicon.icon end,
                 fg = function(buffer) return buffer.devicon.color end,
             },
             {
-                text = function(buffer) return buffer.unique_prefix .. buffer.filename end,
+                text = function(buffer)
+                    return buffer.unique_prefix .. buffer.filename
+                end,
                 bold = function(buffer) return buffer.is_focused end,
                 italic = function(buffer) return buffer.is_modified end,
             },
             {
-                text = function(buffer) return buffer.is_modified and '*' or '' end,
+                text = function(buffer)
+                    return buffer.is_modified and '*' or ''
+                end,
+                italic = true
             },
             {
                 text = '    ',
@@ -41,8 +75,10 @@ return function()
             filetype = 'NvimTree',
             components = {
                 {
-                    text = '',
-                    bg = get_hex('NvimTreeNormal', 'bg'),
+                    text = function()
+                        return vim.fn.fnamemodify(vim.fn.getcwd(), ':~')
+                    end,
+                    bg = get_hex('Pmenu', 'bg'),
                 },
             },
         },
--- lua/config/tree.lua
+++ lua/config/tree.lua
@@ -1,6 +1,17 @@
 return function ()
     require('nvim-tree').setup {
         hijack_cursor = true,
+        view = {
+        },
+        renderer = {
+            group_empty = true,
+            root_folder_label = false,
+        },
     }
+
+    -- Setup hightlights
+    vim.cmd.hi('clear', 'NvimTreeNormal')
+    vim.cmd.hi('link', 'NvimTreeNormal', 'Normal')
+    vim.cmd.hi('link', 'NvimTreeCursorLine', 'PmenuSel')
 end
 
