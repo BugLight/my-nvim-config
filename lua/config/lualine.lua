ca829c6dcffc2019ed37676d34a35e0a8a957b5a
buglight <dmzhukov@outlook.com>
2023-08-26 18:55:20 +0400

    Customize status line

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -2,11 +2,26 @@ return function ()
     require('lualine').setup {
         options = {
             theme = vim.g.colors_name,
+            component_separators = { left = ' ', right = ' ' },
+            section_separators = { left = ' ', right = ' ' },
+            disabled_filetypes = {
+                statusline = {
+                    'NvimTree',
+                },
+            },
+            globalstatus = true,
         },
         extensions = {
-            'nvim-tree',
             'toggleterm',
-        }
+        },
+        sections = {
+            lualine_a = { 'mode' },
+            lualine_b = { 'branch', 'diff' },
+            lualine_c = { 'filename' },
+            lualine_x = { 'encoding' },
+            lualine_y = { { 'diagnostics', always_visible = true } },
+            lualine_z = { 'location' }
+        },
     }
 end
 
