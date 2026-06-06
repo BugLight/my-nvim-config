f3b56e043ff010c77659432eecf78ddcd6dadc36
Dani Zhukov <dmzhukov@outlook.com>
2026-06-06 18:09:45 +0200

    Nix Flake support

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "My Neovim configuration files and dependencies";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs @ {flake-parts, ...}: let
+    my-nvim-config = {
+      stdenv,
+      pkgs,
+    }:
+      stdenv.mkDerivation rec {
+        name = "my-nvim-config";
+        meta.mainProgram = "nvim";
+
+        src = ./.;
+
+        buildInputs = [pkgs.git pkgs.neovim];
+
+        nativeBuildInputs = [pkgs.makeWrapper];
+
+        installPhase = ''
+          mkdir -p $out/${name}
+          cp -rv $src/* $out/${name}
+          makeWrapper ${pkgs.neovim}/bin/nvim $out/bin/nvim \
+              --set XDG_CONFIG_HOME $out \
+              --set NVIM_APPNAME ${name}
+        '';
+      };
+  in
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+      ];
+      systems = ["x86_64-linux" "aarch64-darwin"];
+      perSystem = {pkgs, ...}: {
+        packages.default = pkgs.callPackage my-nvim-config {};
+      };
+    };
+}
